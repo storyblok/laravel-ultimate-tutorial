d18ad64d09b0c84b826033a263f4a5c828c0831b
Roberto Butti <roberto.butti@gmail.com>
2022-09-01 18:04:25 +0200

    Adding components :factory:

---
+++ resources/views/components/feature.blade.php
@@ -0,0 +1,4 @@
+{!! Arr::get($blok, '_editable', '') !!}
+<div class="w-full p-12 bg-[#f7f6fd] rounded-[5px] text-center">
+    {{ $blok["name"] }}
+</div>
---
+++ resources/views/components/page.blade.php
@@ -0,0 +1,6 @@
+{!! Arr::get($blok, '_editable', '') !!}
+<div class="font-[Roboto] px-4">
+    @foreach ($blok['body'] as $blok)
+        <x-dynamic-component :component="$blok['component']" :blok="$blok" class="mt-4" />
+    @endforeach
+</div>
