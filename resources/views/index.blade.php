1ee598a0bb9c2875399586912a5ed70a86f00c9c
Roberto Butti <roberto.butti@gmail.com>
2022-09-06 23:54:04 +0200

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ $story->name }}</title>
-    @vite('resources/css/app.css')
-</head>
-
-<body class="antialiased">
+<x-common.layout :name="$story->name">
     <x-dynamic-component :component="$story->content['component']" :blok="$story->content" class="mt-4" />
+</x-common.layout>
 
-    <script type="text/javascript" src="//app.storyblok.com/f/storyblok-v2-latest.js"></script>
-    <script type="text/javascript">
-        const storyblokInstance = new StoryblokBridge()
-        storyblokInstance.on('change', () => {
-            window.location.reload(true);
-        })
-    </script>
-</body>
-
-</html>
