37f7677d5bfea4515c9aa7a2f2830ef64bef0b98
Roberto Butti <roberto.butti@gmail.com>
2022-09-06 23:53:54 +0200

    Update teaser.blade.php

--- resources/views/components/teaser.blade.php
+++ resources/views/components/teaser.blade.php
@@ -1,4 +1,4 @@
 {!! Arr::get($blok, '_editable', '') !!}
 <div class="py-32 text-6xl text-[#50b0ae] font-bold text-center">
-    {{ $blok["headline"] }}
+    {{ Arr::get($blok, 'headline', '') }}
 </div>
