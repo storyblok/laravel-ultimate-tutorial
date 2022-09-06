1075dc3b35f829f03d752491dced34c3da2c5614
Roberto Butti <roberto.butti@gmail.com>
2022-09-06 23:53:19 +0200

    Create header.blade.php

---
+++ resources/views/components/common/header.blade.php
@@ -0,0 +1,16 @@
+<header class="w-full h-24 bg-[#f7f6fd]">
+  <div class="container h-full mx-auto flex items-center justify-between">
+    <a href="{{url('/')}}" <h1 class="text-[#50b0ae] text-3xl font-bold">Storyblok &#10084; Laravel</h1>
+    </a>
+    <nav>
+      <ul class="flex space-x-8 text-lg font-bold">
+        <li>
+          <x-common.menu_item link="/blog" label="Blog" />
+        </li>
+        <li>
+          <x-common.menu_item link="/about" label="About" />
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
