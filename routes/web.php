874181cd295250b8961ff01b5ff5185625a2e4d7
Roberto Butti <roberto.butti@gmail.com>
2022-09-06 23:54:28 +0200

    better error management in routing file

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+// Catch all route
 Route::get('/{slug?}', function ($slug = 'home') {
+    // Load the Token from the Configuration
     $storyblok = new \Storyblok\Client(config('storyblok.api_key'));
-    $storyblok->editMode(); // always enable draft mode
-    $data = $storyblok->getStoryBySlug($slug)->getBody();
-
+    // Enable the edit mode to load preview content
+    $storyblok->editMode();
+    try {
+        // retrieve the Story by the slug as parameter
+        $response = $storyblok->getStoryBySlug($slug);
+        // accessing the body of the response
+        $data = $response->getBody();
+    } catch (\Storyblok\ApiException $e) {
+        // manage the unauthorized error from API
+        if ($e->getCode() === 401) {
+            abort(401, 'Unauthorized API call. Make sure that you Token is valid');
+        }
+        // if there is error from API, raising 404 page
+        abort(404, $e->getMessage());
+    } catch (Exception $e) {
+        // any other generic exception raises 500 page
+        abort(500, $e->getMessage());
+    }
+    // load the blade page
     return view('index', ['story' => (object) $data['story']]);
 });
