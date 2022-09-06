55aad8ad7dc88f5c5a4845149e78be49c6983ea8
Roberto Butti <roberto.butti@gmail.com>
2022-09-06 23:53:23 +0200

    Create layout.blade.php

---
+++ resources/views/components/common/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{{ $name }}</title>
+  @vite('resources/css/app.css')
+</head>
+
+<body class="antialiased">
+  <x-common.header></x-common.header>
+  {{ $slot }}
+  <script type="text/javascript" src="//app.storyblok.com/f/storyblok-v2-latest.js"></script>
+  <script type="text/javascript">
+    const storyblokInstance = new StoryblokBridge()
+    storyblokInstance.on('change', () => {
+      window.location.reload(true);
+    })
+  </script>
+</body>
+
+</html>
