6eb24e1dca6deb5dd057177af3fcb23980a9bb02
Roberto Butti <roberto.butti@gmail.com>
2023-01-02 22:34:43 +0100

    Showing the Image Field in Blade view

--- resources/views/components/feature.blade.php
+++ resources/views/components/feature.blade.php
@@ -1,4 +1,12 @@
 {!! Arr::get($blok, '_editable', '') !!}
-<div class="w-full p-12 bg-[#f7f6fd] rounded-[5px] text-center">
-  {{ Arr::get($blok, 'name', '') }}
+<div class="w-full bg-[#f7f6fd] rounded-[5px] text-center overflow-hidden">
+  <img
+    src="{{ Arr::get($blok, 'image.filename', '') }}"
+    alt="{{ Arr::get($blok, 'image.alt', '') }}"
+    class="w-full h-48 xl:h-72 object-cover" />
+  <div class="px-12 py-6">
+    <h3 class="text-2xl text-[#1d243d] font-bold">
+      {{ Arr::get($blok, 'name', '') }}
+    </h3>
+  </div>
 </div>
